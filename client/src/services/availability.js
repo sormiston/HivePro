b21b39e504f87f8daa5d9c63c1c3518a63bf96a5
SOrmiston <sean.david.ormiston@gmail.com>
2020-09-18 15:49:57 -0400

    migration editing

---
+++ client/src/services/availability.js
@@ -0,0 +1,4 @@
+import appointmentData from './2020-09-18T00 00 00-04 00.json'
+
+const data = JSON.parse(appointmentData)
+console.log(appointmentData)
--- db/migrate/20200916141358_add_refs_to_band.rb
+++
@@ -1,5 +0,0 @@
-class AddRefsToBand < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :bands, :appointments, null: false, foreign_key: true
-  end
-end
