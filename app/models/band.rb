5d1487231fbc47252f6d71fbc1a8b00e061bb035
SOrmiston <sean.david.ormiston@gmail.com>
2020-09-17 23:28:51 -0400

    appointments well seeded

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,6 @@
 class Band < ApplicationRecord
   # TODO: -- add destroy dependency to line 4
-  has_many :users
-  has_many :appointments
+  has_many :users, dependent: :destroy
+  has_many :appointments, dependent: :destroy
   has_many :rooms, through: :appointments
 end
