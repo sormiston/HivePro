5d135e4b997dd9c9dc974f24c700b6147293d7f9
SOrmiston <sean.david.ormiston@gmail.com>
2020-09-18 11:38:55 -0400

    constructs get appointments API call

--- client/src/components/AppointmentFinder/CheckAvail.jsx
+++ client/src/components/AppointmentFinder/CheckAvail.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import Calendar from './Calendar/Calendar'
 import TimeFilter from './TimeFilter'
-import GnosticDisplay from "../Booker/TimeGnostic/display";
+import GnosticDisplay from '../Booker/TimeGnostic/display'
+import {getAppointments} from '../../services/CRUD'
 import './calendarRootStyles.css'
 
 export default function CheckAvail(props) {
@@ -9,21 +10,25 @@ export default function CheckAvail(props) {
   const [dateWrap, setDateWrap] = useState(currentDateTime)
   const [selectedBooking, setSelectedBooking] = useState({
     booking_hour_start: null,
-    hours_booked: 0
+    hours_booked: 0,
   })
-  
-  
-  
- 
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      const apptArray = await getAppointments(dateWrap.format())
+    }
+    fetchAppointments()
+  }, [dateWrap])
+  const runCheck = async () => {}
+
   return (
     <div>
-    
       <h1>Check availability by date...</h1>
-      
+
       <Calendar value={dateWrap} setValue={setDateWrap} />
+      <button onClick={runCheck}>Check</button>
       <TimeFilter setSelectedBooking={setSelectedBooking} />
       <GnosticDisplay />
-      
     </div>
   )
 }
--- client/src/services/CRUD.js
+++ client/src/services/CRUD.js
@@ -6,3 +6,15 @@ export const getBandNames = async () => {
   const res = await api.get('/bands/names')
   return res.data
 }
+
+// GET /appointments
+
+export const getAppointments = async (datestr) => {
+  console.log(datestr)
+  try {
+    const res = await api.get(`/appointments/${datestr}`)
+    return res.data
+  } catch (err) {
+    return err
+  }
+}
